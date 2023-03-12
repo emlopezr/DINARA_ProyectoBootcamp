417bffc2fce956449093de93e3db0e7bf12bf61c
lopezemmanuel <lopezrodemmanuel@gmail.com>
2023-03-12 14:15:16 -0500

    Dependencias cambiadas

--- src/components/login/LoginPage.jsx
+++ src/components/login/LoginPage.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const LoginPage = () => {
     return (
-        <div>LoginPage</div>
+        <div>
+            <div className="login-form">
+                Hola
+            </div>
+        </div>
     )
 }
 

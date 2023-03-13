4419ee432a283daa8ca7c54f12aff13ae3cc366a
lopezemmanuel <lopezrodemmanuel@gmail.com>
2023-03-12 19:41:55 -0500

    Arreglado error deploy

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename='/DINARA_ProyectoBootcamp'>
             <AppRouter />
         </BrowserRouter>
     </React.StrictMode>,
